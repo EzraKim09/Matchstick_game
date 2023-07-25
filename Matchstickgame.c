ca5e2a35a401af59332af6a22ab4370b723eecaf
K1mCanC0d3 <117889472+EzraKim09@users.noreply.github.com>
2023-07-25 19:21:36 +0530

    Update Matchstickgame.c
    WELCOME TO THE MATCHSTICK GAME
    *There are 21 Matchsticks
    *Pick 1, 2, 3, or 4 Matchsticks
    *Then the computer will pick too
    *The one to pick the last Matchstick lose
    
    Sup, peeps! 🎮 Check out this super basic game I made during my 1st semester of college. 📚 Later on, I jazzed up the UI and made it more playable while also fixing some tricky edge cases. 🛠️ My main goal is to boost my GitHub activity, so I'll be dropping simple codes like this more often. 😎
    
    I really tried to make the game user-friendly, but ngl, you ain't ever gonna win fam. 😅💔

--- Matchstickgame.c
+++ Matchstickgame.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-void game_fixed()
-{
+void game_fixed(){
     int no_ms = 21;
     int per, cpu;
-    while(no_ms > 0)
-    {
+    while(no_ms > 0){
         printf("\nNumber of Matchstick left is %d", no_ms);
         printf("\nPick 1, 2, 3, or 4\nPick:");
         scanf("%d", &per);
@@ -24,15 +22,13 @@ void game_fixed()
 
 
 
-int main()
-{
+int main(){
     char opt, noise;            
     printf("\n\nWELCOME TO THE MATCHSTICK GAME\n\n*There are 21 Matchsticks\n*Pick 1, 2, 3, or 4 Matchsticks\n*Then the computer will pick too\n*The one to pick the last Matchstick lose\n\n");
     printf("Press enter to start\n");
     scanf("%c", &opt);
     game_fixed();                    
-    while(1)                   
-    {
+    while(1){
         printf("\n\nPress enter to play again(Enter Q to quit):\n");
         scanf("%c", &noise);                
         scanf("%c", &opt);
